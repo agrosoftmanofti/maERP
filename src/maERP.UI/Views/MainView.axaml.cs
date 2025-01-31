a551e7bbf7ef65e070382efc17ea3eb970d0bb1b
Martin <ma@martin-andrich.de>
2025-01-31 13:47:45 +0100

    add avalonia

---
+++ src/maERP.UI/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace maERP.UI.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
